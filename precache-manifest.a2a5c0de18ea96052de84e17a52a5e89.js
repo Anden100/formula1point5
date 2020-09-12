aa4b2aca400a6129f253c34c4b18fb554ba5136f
Anders Coordt Poulsen <anders@Anderss-MacBook-Pro.local>
2020-09-12 22:41:42 +0200

    Updates

--- precache-manifest.a2a5c0de18ea96052de84e17a52a5e89.js
+++ precache-manifest.a2a5c0de18ea96052de84e17a52a5e89.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5ddf75068d53af3d3d99fd43a09a022f",
+    "revision": "935c7f3bb448633aab198ff7395ea4e8",
     "url": "/formula1point5/index.html"
   },
   {
-    "revision": "4507a2961f5038276d4f",
-    "url": "/formula1point5/static/css/main.ce875d73.chunk.css"
+    "revision": "0e67a20843c0f34e35e0",
+    "url": "/formula1point5/static/css/main.946b6695.chunk.css"
   },
   {
     "revision": "6fe04b9cc7f0b58f3b3e",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/formula1point5/static/js/2.23c85037.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4507a2961f5038276d4f",
-    "url": "/formula1point5/static/js/main.796fde4b.chunk.js"
+    "revision": "0e67a20843c0f34e35e0",
+    "url": "/formula1point5/static/js/main.0b420ed1.chunk.js"
   },
   {
     "revision": "fe7a2f078d6130bcf457",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/formula1point5/precache-manifest.15e145faf4dcfe00040767fe7e8614a5.js"
+  "/formula1point5/precache-manifest.a2a5c0de18ea96052de84e17a52a5e89.js"
 );
 
 self.addEventListener('message', (event) => {
