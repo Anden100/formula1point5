38b1aae528278b36d3739cbc28cf643c30a9be48
Anders Coordt Poulsen <anders@Anderss-MacBook-Pro.local>
2020-09-12 15:01:35 +0200

    Updates

---
+++ precache-manifest.b80ad2de45f2fa787c98ca3bc9ed6524.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fed486158067146c2da7514f3acaf87",
+    "url": "/formula1point5/index.html"
+  },
+  {
+    "revision": "4d897549d59632a9e066",
+    "url": "/formula1point5/static/css/main.899c325d.chunk.css"
+  },
+  {
+    "revision": "3229c2afa051b86e772a",
+    "url": "/formula1point5/static/js/2.c2cfda49.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/formula1point5/static/js/2.c2cfda49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d897549d59632a9e066",
+    "url": "/formula1point5/static/js/main.8a240104.chunk.js"
+  },
+  {
+    "revision": "fe7a2f078d6130bcf457",
+    "url": "/formula1point5/static/js/runtime-main.326034bf.js"
+  }
+]);
